d55272908821f2ac88d3adaadf4f79266022ebca
Armando Lopez <722925@student.dusd.net>
2024-03-14 11:42:30 -0700

    Added move forwaard script
    made it so i can throw the cookie

--- Unit2ChallengeP3Lopez/Assets/Scripts/PlayerController.cs
+++ Unit2ChallengeP3Lopez/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +27,10 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
